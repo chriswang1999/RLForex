7cab95e8ee5f2784dc223caf182286b5aabe7b80
IrisQY <iriswangsu@gmail.com>
2019-05-18 14:07:38 -0700

    pro_data

--- DQN/environment.py
+++ DQN/environment.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-import datetime
+# import datetime
+# from pro_data import CreateFeature
 
-Pad = pd.read_csv('PadData_v2.csv')
-to_draw = np.sort(Pad['timestamp'].unique())
-to_draw_train = to_draw[:int(to_draw.shape[0]*0.6)]
-to_draw_eval = to_draw[int(to_draw.shape[0]*0.6):int(to_draw.shape[0]*0.8)]
-to_draw_test = to_draw[int(to_draw.shape[0]*0.8):]
-currency = list(np.sort(Pad['currency pair'].unique()))
+# Pad = pd.read_csv('PadData_v2.csv')
+# currency = list(np.sort(Pad['currency pair'].unique()))
 
 class Environment(object):
     """
@@ -39,9 +36,9 @@ class ForexEnv(Environment):
             bid, ask price of target currency
 
     Actions:
-        -1  short
-        0   neutral
-        1   long
+        0  short
+        1   neutral
+        2   long
 
     Starting State:
         random start within training set
@@ -54,90 +51,63 @@ class ForexEnv(Environment):
         self.ccy = cur
         self.lag = lag
         self.min_history = min_history
-        self.timestamp = None
+        self.index = None
         self.state = None
         self.price_record = None
+        # self.df = CreateFeature(self.ccy, self.lag).reset_index(drop = True)
+        filename = self.ccy + '_lag_' + str(self.lag) + '.csv'
+        self.df = pd.read_csv(filename).reset_index(drop = True)
+        self.totalframe = self.df.index.values.tolist()
+        self.train = self.totalframe[:int(0.6*len(self.totalframe))-self.min_history]
+        self.eval = self.totalframe[int(0.6*len(self.totalframe)):int(0.8*len(self.totalframe))-self.min_history]
+        self.test = self.totalframe[int(0.8*len(self.totalframe)):]
 
-    def features(self,price_path):
-        features = np.zeros(self.lag)
-        for i in range(self.lag):
-            features[i] = np.log(price_path[-1]) - np.log(price_path[-(i+2)])
-        return features
+    def get_features(self,_idx):
+        colindex = range(9,9 + self.lag * 9 + 4)
+        bid = self.df['bid price'].values[_idx]
+        ask = self.df['ask price'].values[_idx]
+        # feature_span = self.df[colindex].to_numpy()[_idx,:]
+        feature_span = self.df.iloc[_idx,9:].values
+        return bid, ask, feature_span
 
-    def get_features(self, timestamp, currency = currency):
-        tmp = Pad[Pad['currency pair'] == self.ccy]
-        idx = to_draw_train.tolist().index(timestamp)
-        timeframe = to_draw_train[idx - self.lag:idx+1]
-        b_target = tmp[tmp.timestamp.isin(timeframe)]['bid price'].values
-        a_target = tmp[tmp.timestamp.isin(timeframe)]['ask price'].values
-        feature_span = self.features(b_target)
-        feature_span = np.append(feature_span, self.features(a_target), axis = 0)
-        for cur in currency:
-            if cur == self.ccy:
-                pass
-            else:
-                _tmp = Pad[Pad['currency pair'] == cur]
-                b_other = _tmp[_tmp.timestamp.isin(timeframe)]['bid price'].values
-                a_other = _tmp[_tmp.timestamp.isin(timeframe)]['ask price'].values
-                mid_other = np.mean(np.asarray([b_other,a_other]), axis=0)
-                feature_span = np.append(feature_span, self.features(mid_other), axis = 0)
-        return feature_span
-
-    def step(self, action):
+    def step(self, action, mode = 'train'):
         assert action in [0, 1, 2], "invalid action"
-        time = self.timestamp
-        idx = to_draw_train.tolist().index(time)+1
-        timestamp = to_draw_train[idx]
-
-        tmp = Pad[Pad['currency pair'] == self.ccy]
+        self.index += 1
         position = np.zeros(3)
         position[action] = 1
 
-        dow = datetime.datetime(int(timestamp[6:10]),int(timestamp[0:2]),int(timestamp[3:5])).weekday()
-        h = int(timestamp[11:13])
-        m = int(timestamp[14:16])
-        s = int(timestamp[17:19])
-        state_0 = np.asarray([np.sin(np.pi*s/60),np.sin(np.pi*m/60),np.sin(np.pi*h/24),np.sin(np.pi*dow/7)])
-        state_1 = self.get_features(timestamp)
-        state_2 = position
+        bid, ask, feature_span = self.get_features(self.index)
+        self.state = np.append(feature_span,position, axis = 0)
+        self.price_record = (bid,ask)
+        return self.index, self.state, self.price_record, False
 
-        self.timestamp = timestamp
-        self.state = np.append(np.append(state_0,state_1,axis = 0),state_2, axis = 0)
-        self.price_record = (tmp[tmp.timestamp == timestamp]['bid price'].values[0],
-                             tmp[tmp.timestamp == timestamp]['ask price'].values[0])
-        
-        return self.timestamp, self.state, self.price_record, False
+    def reset(self, mode = 'train'):
+        if mode == 'train':
+            to_draw = self.train
+        elif mode == 'eval':
+            to_draw = self.eval
+        elif mode == 'test':
+            to_draw = self.test
+        n = np.random.choice(len(to_draw))
+        self.index = to_draw[n]
 
-    def reset(self):
-        n = np.random.random_integers(self.lag+1,to_draw_train.shape[0] - self.min_history)
-        timestamp = to_draw_train[n]
-
-        tmp = Pad[Pad['currency pair'] == self.ccy]
         position = np.zeros(3)
-        position[np.random.randint(3)] = 1
+        action = np.random.choice(3)
+        position[action] = 1
 
-        dow = datetime.datetime(int(timestamp[6:10]),int(timestamp[0:2]),int(timestamp[3:5])).weekday()
-        h = int(timestamp[11:13])
-        m = int(timestamp[14:16])
-        s = int(timestamp[17:19])
-        state_0 = np.asarray([np.sin(np.pi*s/60),np.sin(np.pi*m/60),np.sin(np.pi*h/24),np.sin(np.pi*dow/7)])
-        state_1 = self.get_features(timestamp)
-        state_2 = position
-
-        self.timestamp = timestamp
-        self.state = np.append(np.append(state_0,state_1,axis = 0),state_2, axis = 0)
-        self.price_record = (tmp[tmp.timestamp == timestamp]['bid price'].values[0],
-                             tmp[tmp.timestamp == timestamp]['ask price'].values[0])
-        return self.timestamp, self.state, self.price_record
+        bid, ask, feature_span = self.get_features(self.index)
+        self.state = np.append(feature_span,position, axis = 0)
+        self.price_record = (bid,ask)
+        return self.index, self.state, self.price_record
 
 
 ### test
 if __name__=='__main__':
-    nsteps = 2
+    nsteps = 5
     np.random.seed(0)
 
     env = ForexEnv()
-    time, obs,price = env.reset()
+    time, obs, price = env.reset()
     t = 0
     print(time)
     print(obs.shape)
---
+++ DQN/pro_data.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+def CreateFeature(cur, lag):
+    Pad = pd.read_csv('PadData_v2.csv')
+    currency = list(np.sort(Pad['currency pair'].unique()))
+    tmp = Pad[Pad['currency pair'] == cur].sort_values(by=['timestamp'])
+    for i in range(1,lag+1):
+        colname1 = 'bid_lag_' + str(i)
+        colname2 = 'ask_lag_' + str(i)
+        tmp[colname1] = np.log(tmp['bid price']) - np.log(tmp['bid price'].shift(i))
+        tmp[colname2] = np.log(tmp['ask price']) - np.log(tmp['ask price'].shift(i))
+    for ccy in currency:
+        if ccy == cur:
+            pass
+        else:
+            _tmp = Pad[Pad['currency pair'] == ccy].sort_values(by=['timestamp'])
+            mid =  pd.DataFrame(np.mean(np.asarray([_tmp['bid price'].values,_tmp['ask price'].values]), axis=0))
+            for i in range(1,lag+1):
+                colname3 = ccy + '_lag_' + str(i)
+                tmp[colname3] = np.log(mid) - np.log(mid.shift(i))
+    tmp['date'] = tmp['timestamp'].astype(str).str[0:10]
+    tmp['dow'] = pd.to_datetime(tmp['date']).dt.dayofweek
+    tmp['hh'] = tmp['timestamp'].astype(str).str[11:13]
+    tmp['mm'] = tmp['timestamp'].astype(str).str[14:16]
+    tmp['ss'] = tmp['timestamp'].astype(str).str[17:19]
+    tmp['time_1'] = np.sin(np.pi*tmp['dow'].values/7)
+    tmp['time_2'] = np.sin(np.pi*tmp['hh'].astype('int64').values/24)
+    tmp['time_3'] = np.sin(np.pi*tmp['mm'].astype('int64').values/60)
+    tmp['time_4'] = np.sin(np.pi*tmp['ss'].astype('int64').values/60)
+    tmp = tmp.drop(['date', 'dow','hh','mm','ss'], axis=1)
+    tmp = tmp.reset_index(drop=True)
+    tmp = tmp[lag:]
+    filename = cur + '_lag_' + str(lag) + '.csv'
+    tmp.to_csv(filename ,index=False)
+
+if __name__=='__main__':
+    CreateFeature('AUDUSD', 16)
