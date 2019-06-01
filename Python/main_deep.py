1dc767e8874d104e9a93d1d4d5b3eee5290ac435
beehamer <ylxu@stanford.edu>
2019-06-01 16:29:51 -0700

    add a new structure and update a flag in main_deep.py

--- Python/main_deep.py
+++ Python/main_deep.py
@@ -1,11 +1,10 @@
 import os
-from run_deep_gpu import train_eval
-from test_deep_gpu import test
+import sys
+
 import time
 import argparse
 
-reward_file = './deep/reward'+ time.strftime("%Y%m%d-%H%M%S") + '.txt'
-
+reward_file = './deep/reward'+ str(time.time()) + '.txt'
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--mode', type=str, default='train_eval')
@@ -13,6 +12,7 @@ parser.add_argument('--save', type=str, default='RLFX')
 
 parser.add_argument('--reward_file', type=str, default=reward_file)
 
+parser.add_argument('--model_structure', type=int, default=0)
 
 parser.add_argument('--currency', type=str, default='AUDUSD')
 parser.add_argument('--min_history', type=int, default=1000)
@@ -31,7 +31,14 @@ parser.add_argument('--offset', type=int, default=300)
 parser.add_argument('--num_of_test', type=int, default=50)
 
 config = parser.parse_args()
-
+if config.model_structure == 0:
+    from run_deep_gpu import train_eval
+    from test_deep_gpu import test
+else if config.model_structure == 1:
+    from run_deep_gpu_structure_2 import train_eval
+    from test_deep_gpu_stucture_2 import test
+else:
+    raise Exception('Model structure has to be specified')
 if config.mode == 'train_eval':
     train_eval(config)
 elif config.mode == 'test':
