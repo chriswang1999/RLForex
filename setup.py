f849f8ba3a5cbe0e591a90265cc8ec68af57b93b
Yuqin Dai <dyq4430@126.com>
2019-06-08 14:32:09 -0700

    change files

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-setup(name='gym_banana',
-      version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'pandas',
-                        'cfg_load']
-      )
